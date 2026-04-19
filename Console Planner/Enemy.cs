dda54b15337bd8ad7f2fd8e11b9dee0ce6bfbe51
reb1t <fgkkhbgf@gmail.com>
2026-04-19 19:13:37 +0500

    Создан новый класс Enemy

---
+++ Console Planner/Enemy.cs
@@ -0,0 +1,11 @@
+class Enemy
+{
+    private string name;
+    private int health;
+    private int maxHealth;
+    private int goldReward;
+    private int expReward;
+    private int minDamage;
+    private int maxDamage;
+    static Random rand = new Random();
+}
